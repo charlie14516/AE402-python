3951ea2201a3738f8778684271422153a8ebb84a
charlie14516 <78331755+charlie14516@users.noreply.github.com>
2021-02-01 13:58:27 +0800

    Add files via upload

---
+++ 圓面積 周長.py
@@ -0,0 +1,6 @@
+r=input("請使用者輸入半徑")
+r=float(r)
+Area=r**2*3.14159
+l=2*r*3.14159
+print("圓周長",l)
+print("園面積",Area)
---
+++ 成績鑑定.py
@@ -0,0 +1,6 @@
+score=input('請輸入成績')
+score=float(score)
+if (score>=60):
+    print('及格')
+else:
+    print('不及格')
