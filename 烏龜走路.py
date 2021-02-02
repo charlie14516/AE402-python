33176b2bd73543ccce8f9a31ef936101c685f35d
charlie14516 <78331755+charlie14516@users.noreply.github.com>
2021-02-02 14:24:49 +0800

    Add files via upload

---
+++ 烏龜走路.py
@@ -0,0 +1,11 @@
+import turtle
+sky=turtle.Screen()
+bill=turtle.Turtle()
+bill.shape( 'turtle' )
+bill.color( 'red' )
+sky.title( 'my window' )
+sky.bgcolor( 'blue' )
+while True:
+  bill.forward(200)
+  bill.left(90)
+  
