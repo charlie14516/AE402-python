32f5f2c0b57fd27099b33e07153d7daa9275f9f3
charlie14516 <78331755+charlie14516@users.noreply.github.com>
2021-02-03 15:23:05 +0800

    Add files via upload

---
+++ 函數.py
@@ -0,0 +1,8 @@
+def sumnumber(num1, num2, num3):
+    n=num1+num2+num3
+    print(n)
+    
+num1 = int(input("請輸入第一個數字"))
+num2 = int(input('請輸入第二個數字'))
+num3 = int(input('請輸入第三個數字'))
+sumnumber(num1, num2, num3)
